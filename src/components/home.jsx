5a41aa568db52b55878ff3e9a9f0c420bb015a0d
Daniel Tolani <mumytosin2016@gmail.com>
2023-03-30 06:14:45 +0100

    Landing-page hero section 50% done

---
+++ src/components/home.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../styles/home.css";
+import { useState } from "react";
+
+const Home = () => {
+  const [x, setX] = useState("50%");
+  const [y, setY] = useState("50%");
+
+  const handleMouseMove = (e) => {
+    const rect = e.target.getBoundingClientRect();
+    const posX = e.clientX - rect.left;
+    const posY = e.clientY - rect.top;
+    setX(`${posX}px`);
+    setY(`${posY}px`);
+  };
+
+  return (
+    <section className="home-section">
+      <div className="home">
+        <div className="hero-section">
+          <h1 className="hero-heading home-center">
+            The Next Evolution of Social Media
+          </h1>
+          <p className="hero-paragraph home-center">
+            Discover the latest trends in content creation, gaming, investing,
+            and more.
+          </p>
+          {/* <button className="btn home-center">Join Xviral today</button> */}
+          <button
+            className="button"
+            style={{
+              "--x": x,
+              "--y": y,
+            }}
+            onMouseMove={handleMouseMove}
+          >
+            Join Xviral today
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
